c53aad93ba642b94180f2e68ad580a6eec190fce
lhourquin <lhourquin@gmail.com>
2020-06-05 16:29:25 +0200

    make login

--- connection.php
+++ connection.php
@@ -1,4 +1,40 @@
 <?php
+session_start();
+// require file
+require('config/db.php');
+require('includes/functions.php');
 require('includes/constants.php');
+    // SI LE FORMULAIRE EST SOUMIS
+    if(isset($_POST['connection'])){
+
+        // SI TOUS LES CHAMLPS SONT REMPLIS
+        if(not_empty(['user', 'password'])){
+            extract($_POST);
+
+            $query = $db->prepare("SELECT idUser FROM users 
+                                   WHERE (pseudo = :user OR mail = :user) 
+                                   AND password = :password
+                                   AND active = '1'");
+
+            $query->execute([
+                'user' => $user,
+                'password' => sha1($password)
+            ]);
+
+            $userHasBeenFound = $query->rowCount();
+
+            if($userHasBeenFound){
+                redirect('profile.php');
+            }else{
+                set_flash('Your Pseudo/Mail or Password is incorrect.', 'danger');
+                save_input_data();
+            }
+        }
+            
+    }else {
+        clear_input_data();
+    }
+?>
+<?php
 require('views/connection.view.php');
 ?>
--- views/connection.view.php
+++ views/connection.view.php
@@ -3,6 +3,35 @@ $title = "Connection";
 require('includes/functions.php');
 require('partials/_head.php');
 ?>
+<div class="container text-center">
+    <h1>Connection</h1>
+    <div class="container-fluid col-md-5">
+        <div>
+            <div  id="form-conection">
+                <form data-parsley-validate method="POST" autocomplete="off">
+                    <div class="form-group">
+                        <label for="user">Email address</label>
+                        <input type="text" class="form-control" id="user" name="user" aria-describedby="emailHelp" placeholder="Enter email or Pseudo" value="<?= get_input('user')?>" required>
+                        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                    </div>
+                    <!--
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    -->
+                    <button type="submit" id="submitConection" class="btn btn-primary" name="connection">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    
+</div>
 
 <?php
 require('partials/_footer.php');
